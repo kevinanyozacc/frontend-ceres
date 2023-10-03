55aac7bf1bfad9325503f296649d4bf0d622ef23
Hans Medina <twd2206@gmail.com>
2023-10-03 16:23:30 -0500

    show detail table

--- src/shared/table/components/table-simple-cell.jsx
+++ src/shared/table/components/table-simple-cell.jsx
@@ -4,13 +4,13 @@ const defaultProps = {
   align: "left",
   colSpan: 1,
   simple: false,
-  onclick: () => null,
+  onClick: () => null,
 };
 
 export function TableSimpleCell(props = defaultProps) {
   return (
     <td
-      onClick={props.onclick}
+      onClick={props.onClick}
       colSpan={props.colSpan || 1}
       className={`
         cell 
